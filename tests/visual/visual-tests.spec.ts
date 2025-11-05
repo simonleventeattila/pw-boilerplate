dd6e1011971deacd7263376dec989a84071eb86c
Simon Levente-Attila <simonleventeattila@gmail.com>
2025-11-05 22:41:07 +0200

    feat: add visual test for product page

---
+++ tests/visual/visual-tests.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '../../base';
+
+test.describe('Test pages visually (screenshot matching)', () => {
+  test.beforeEach(async ({ page, loginPage }) => {
+    await page.goto('/');
+    await loginPage.doLogin();
+  });
+
+  test('Test product page visually', async ({ page }) => {
+    await expect(page.locator('[data-test="title"]')).toContainText('Products');
+    await expect(page).toHaveScreenshot();
+  });
+});
