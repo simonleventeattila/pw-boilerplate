e38f00c8e8d2238e9b137968f5a0f6772e54ffbc
Simon Levente-Attila <simonleventeattila@gmail.com>
2025-11-06 19:09:34 +0200

    feat: add get single product test

--- tests/api/get-products.spec.ts
+++ tests/api/get-products.spec.ts
@@ -5,11 +5,23 @@ import { validateSchemaZod } from 'playwright-schema-validator';
 const baseUrl = 'https://api.practicesoftwaretesting.com';
 
 test.describe('Get verb tests', () => {
+  let dataId = '';
   test('Get products test', async ({ request, page }) => {
     const response = await request.get(`${baseUrl}/products`);
     const respBody = await response.json();
     expect(response.status()).toBe(200);
+    dataId = respBody.data[0].id;
 
     await validateSchemaZod({ page }, respBody, getProductsSchema);
+  });
+
+  test('Get product', async ({ request, page }) => {
+    const response = await request.get(`${baseUrl}/products/${dataId}`);
+    const respBody = await response.json();
+
+    expect(response.status()).toBe(200);
+    expect(respBody.name).toBe('Combination Pliers');
+    expect(typeof respBody.price).toBe('number');
+    expect(typeof respBody.price).toBeFalsy;
   });
 });
